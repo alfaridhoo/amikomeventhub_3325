8e0e770b4a19e54bb4e4b9094d0e01d079b0dc02
Alfaa <muhammadalfaridho6433@gmail.com>
2026-04-10 07:36:14 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return '<h1>Ini adalah Halaman Tentang Aplikasi Event HUb</h1>';
 });
+
+Route::get('/kontak', function () {
+    return view('contatct');
+});
