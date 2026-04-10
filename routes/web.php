587f8f1c2f3b1237c5282f03ee707320c3f8a8d0
Alfaa <muhammadalfaridho6433@gmail.com>
2026-04-10 09:19:51 +0700

    Tugas 1

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,15 @@ Route::get('/', function () {
 Route::get('/kontak', function () {
     return view('contact');
 });
+
+Route::get('/profil', function () {
+    return view('profil');
+});
+
+Route::get('/katalog', function () {
+    return view('katalog');
+});
+
+Route::get('/bantuan', function () {
+    return view('bantuan');
+});
