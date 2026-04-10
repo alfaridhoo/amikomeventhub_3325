4193dcd75c40cfc5ee85b2017eff33747db98dbd
Alfaa <muhammadalfaridho6433@gmail.com>
2026-04-10 20:29:55 +0700

    Tugas 1

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return '<h1>Ini adalah Halaman Tentang Aplikasi Event HUb</h1>';
+    return '<h1>Ini adalah Halaman Tentang Aplikasi Event HUb Selamat Datang</h1>';
 });
 
 Route::get('/kontak', function () {
